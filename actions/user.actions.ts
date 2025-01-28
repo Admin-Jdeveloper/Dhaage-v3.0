93a5cb431fbb76f595433cf1659ff682ed9c36f5
admin-jdeveloper <jdeveloper.site@gmail.com>
2025-01-28 20:06:26 +0530

    user.action

--- actions/user.actions.ts
+++ actions/user.actions.ts
@@ -2,73 +2,66 @@
 "use server"
 // import { PrismaClient } from "@prisma/client";
 import  prisma  from "@/lib/prismasingleton";
-import { cookies } from "next/headers";
 
 
 
 
-export const signup = async (userdata : object) => {
+
+export const signup = async (userdata) => {
 
 //  const prisma = new PrismaClient()
 
-    const cookieStore = await cookies();
+
 console.log(userdata)
 
 const jj = await prisma.user.create({
 
 data :{
 
-name : userdata.NAME,
-password : userdata.PASSWORD,
-email : userdata.Gmail
+name : userdata?.NAME,
+password : userdata?.PASSWORD,
+email : userdata?.Gmail
 
 }
 
 })
-cookieStore.set({
-    name: "userToken",         // Cookie name
-    value: jj.id,     // Cookie value
-    httpOnly: true,            // Prevent client-side access
-    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-    path: "/",                 // Cookie is available site-wide
-    maxAge: 60 * 60 * 24 ,  // 7 days in seconds
-  });
+
 return ;
 
 }
 
 
-export const signin = async (userdata : object) => {
+// export const signin = async (userdata : object) => {
  
-    const cookieStore = await cookies();
-    console.log(userdata)
+//     const cookieStore = await cookies();
+//     console.log(userdata)
     
-    const jj = await prisma.user.findUnique({
+//     const jj = await prisma.user.findUnique({
     
-    where :{
+//     where :{
     
    
-    password : userdata.PASSWORD,
-    email : userdata.Gmail
+//     password : userdata.PASSWORD,
+//     email : userdata.Gmail
     
-    }
+//     }
     
-    })
-if(!jj){
-    return {message:"inauthorized"} ;
-}
+//     })
+// if(!jj){
+//     return {message:"inauthorized"} ;
+// }
 
 
-   cookieStore.set({
-        name: "userToken",         // Cookie name
-        value: jj.id,     // Cookie value
-        httpOnly: true,            // Prevent client-side access
-        secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-        path: "/",                 // Cookie is available site-wide
-        maxAge: 60 * 60 * 24 ,  // 7 days in seconds
-      });
+//    cookieStore.set({
+//         name: "userToken",         // Cookie name
+//         value: jj.id,     // Cookie value
+//         httpOnly: true,            // Prevent client-side access
+//         secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+//         path: "/",                 // Cookie is available site-wide
+//         maxAge: 60 * 60 * 24 ,  // 7 days in seconds
+//       });
 
     
     
-    return {message:"authorized"};
-    }
+//     return {message:"authorized"};
+//     }
