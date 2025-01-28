d19954d06421826bd84a7b26bf4bd0a1ce21f38b
admin-jdeveloper <jdeveloper.site@gmail.com>
2025-01-28 20:36:00 +0530

    h

--- app/authprovider.tsx
+++ app/authprovider.tsx
@@ -1,11 +1,20 @@
 "use client"
 
-import { SessionProvider} from "next-auth/react"
 
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
 
+/**
+ * Authentication wrapper for providing session context.
+ *
+ * @param {Object} props
+ * @param {ReactNode} props.children - The components that require access to the session context.
+ * @returns {JSX.Element}
+ */
+interface AuthProps {
+  children: ReactNode;
+}
 
-export default function Auth  ({children} ){
-
-return <SessionProvider>{children}</SessionProvider>
-
+export default function Auth({ children }: AuthProps) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
